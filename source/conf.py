eb4b2e74cdb54bdc8afc98a56e0153a4475facc4
CS <cs@wahdis.com>
2017-09-11 01:56:43 -0400

    Change project name

--- source/conf.py
+++ source/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# techdoc documentation build configuration file, created by
+# technotes documentation build configuration file, created by
 # sphinx-quickstart on Sat Aug 19 14:25:30 2017.
 #
 # This file is execfile()d with the current directory set to its
@@ -14,6 +14,7 @@
 
 import sys
 import os
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -50,7 +51,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'techdoc'
+project = u'technotes'
 copyright = u'2017, Chris'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -105,19 +106,24 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'display_version': False,
+    'sticky_navigation': True
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = [
+    sphinx_rtd_theme.get_html_theme_path()
+]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = 'technotes'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -157,7 +163,7 @@ html_static_path = ['_static']
 #html_additional_pages = {}
 
 # If false, no module index is generated.
-#html_domain_indices = True
+html_domain_indices = False
 
 # If false, no index is generated.
 #html_use_index = True
@@ -183,7 +189,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'techdocdoc'
+htmlhelp_basename = 'technotes'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -203,7 +209,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'techdoc.tex', u'techdoc Documentation',
+  ('index', 'technotes.tex', u'Techdoc Documentation',
    u'Chris', 'manual'),
 ]
 
@@ -233,7 +239,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'techdoc', u'techdoc Documentation',
+    ('index', 'technotes', u'Technotes Documentation',
      [u'Chris'], 1)
 ]
 
@@ -247,8 +253,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'techdoc', u'techdoc Documentation',
-   u'Chris', 'techdoc', 'One line description of project.',
+  ('index', 'technotes', u'Technotes Documentation',
+   u'Chris', 'technotes', 'One line description of project.',
    'Miscellaneous'),
 ]
 
--- source/index.rst
+++ source/index.rst
@@ -1,10 +1,6 @@
-.. techdoc documentation master file, created by
-   sphinx-quickstart on Sat Aug 19 14:25:30 2017.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
 
-Welcome to techdoc's documentation!
-===================================
+Technical Notes
+===============
 
 Contents:
 
@@ -19,6 +15,4 @@ Indices and tables
 ==================
 
 * :ref:`genindex`
-* :ref:`modindex`
 * :ref:`search`
-
